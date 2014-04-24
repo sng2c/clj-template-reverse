24051fbf651c9a2d9db71463203aba07d94d0bb0
sng2c <sng2nara@gmail.com>
2014-04-24 21:33:58 +0900

    change some code more elegant

--- src/template_reverse/core.clj
+++ src/template_reverse/core.clj
@@ -7,17 +7,17 @@
     (let [a (java.util.ArrayList. (vec src))  b (java.util.ArrayList. (vec dst)) ]
         (let [p (difflib.DiffUtils/diff a b)] 
             (map 
-                (fn [itm] {
-                    :type (keyword (str (.getType itm))) 
-                    :pos  (.getPosition (.getOriginal itm))
-                    :len  (.size (.getLines (.getOriginal itm)))
-                     }) 
+                #(hash-map
+                    :type (keyword (str (.getType %))) 
+                    :pos  (.getPosition (.getOriginal %))
+                    :len  (.size (.getLines (.getOriginal %) ))) 
                 (.getDeltas p)))))
 
 
 (defn- -diff-index [col] 
     (flatten 
-        (map #(range (:pos %) (+ (:pos %) (:len %))) 
+        (map 
+            #(range (:pos %) (+ (:pos %) (:len %))) 
             col)))
 
 
@@ -29,20 +29,23 @@
 (defn- -reduce-wildcard [idxs]
     (map 
         first
-        (filter #(not (and (apply = %) (= :* (first %) ) ) ) 
+        (filter 
+            #(not (and (apply = %) (= :* (first %) ) ) ) 
             (partition 2 1 (conj idxs :EOF) ))))
 
 (defn diff 
     "Get diff between Seq."
     [src dst] 
     (let [g (filter #(not (= (:type %) :INSERT)) (-diff src dst))] 
-        (-reduce-wildcard (-set-wildcard src (-diff-index g))))      )
+        (-reduce-wildcard (-set-wildcard src (-diff-index g)))))
 
 (defn detect 
     "Get chunks with BEGIN,END and limited size by 'len' from 'd'."
     [d len]
     (map 
-        #(hash-map :BEGIN (take-last len (first %)), :END (take len (last %))) 
+        #(hash-map 
+            :BEGIN (take-last len (first %))
+            :END (take len (last %))) 
         (partition 3 2 
             (partition-by #(= % :*) 
                 (flatten [:BOF d :EOF])))))
