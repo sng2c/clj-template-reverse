cfd778f9602d58b6e1b335b26c20cebecb647d83
sng2c <sng2nara@gmail.com>
2014-04-24 13:57:18 +0900

    Reformat code

--- src/template_reverse/core.clj
+++ src/template_reverse/core.clj
@@ -1,64 +1,55 @@
 (ns template-reverse.core
-  (:import [difflib])
-  (:gen-class))
+  (:import [difflib]) )
 
+(declare diff-detect detect diff -diff -diff-index -set-wildcard -reduce-wildcard )
 
-
-(defn -main []
-  (println "Hello, World!"))
-
-(declare detect -detect diff -diff -diff2 -set-wildcard -reduce-wildcard )
-
-
-(defn detect [col1 col2 len] 
-    (-detect (diff col1 col2) len)
-)
-
-(defn -detect [d len]
-    (map #(hash-map :left (take-last len (first %)), :right (take len (last %))) (partition 3 2 (partition-by #(= % :*) (flatten (conj (conj [:BOF] d) :EOF))) ))
-)
-
-(defn diff [col1 col2] 
-    
-    (let [g (filter #(not (= (:type %) :INSERT)) (-diff col1 col2))] 
-        (-reduce-wildcard (-set-wildcard col1 (-diff2 g)))
-    )
-
-    
-)
-
-(defn -diff2 [col] 
-    (flatten (map #(range (:pos %) (+ (:pos %) (:len %))) col))
-)
-
-(defn -diff [col1 col2] 
-    (let [a (java.util.ArrayList. (vec col1))  b (java.util.ArrayList. (vec col2)) ]
+(defn- -diff [src dst] 
+    (let [a (java.util.ArrayList. (vec src))  b (java.util.ArrayList. (vec dst)) ]
         (let [p (difflib.DiffUtils/diff a b)] 
-        
             (map 
                 (fn [itm] {
-                    :type (keyword (str (.getType itm))) 
-                    :pos (.getPosition (.getOriginal itm))
-                    :len (.size (.getLines (.getOriginal itm)))
+                    :type (keyword (str         (.getType itm))) 
+                    :pos  (.getPosition      (.getOriginal itm))
+                    :len  (.size (.getLines (.getOriginal itm)))
                      }) 
-                (.getDeltas p)
-            )
-        
-        )
-    
-    )
-)
+                (.getDeltas p)))))
 
-(defn -reduce-wildcard [col]
+
+(defn- -diff-index [col] 
+    (flatten (map #(range (:pos %) (+ (:pos %) (:len %))) col)))
+
+
+(defn- -reduce-wildcard [idxs]
     (map 
         first
         (filter #(not (and (apply = %) (= :* (first %) ) ) ) 
-            (partition 2 1 (conj col :EOF) ) 
+            (partition 2 1 (conj idxs :EOF) ) 
         )   
     )   
 )
 
 
-(defn -set-wildcard [org idxs]
+(defn- -set-wildcard [org idxs]
     (reduce (fn [a b] (assoc a b :*)  ) (vec org) idxs)
+)
+
+
+(defn diff 
+    "Get diff between Seq."
+    [src dst] 
+    (let [g (filter #(not (= (:type %) :INSERT)) (-diff src dst))] 
+        (-reduce-wildcard (-set-wildcard src (-diff-index g)))
+    )      
+)
+
+(defn detect 
+    "Get chunks with BEGIN,END and limited size by 'len' from 'd'."
+    [d len]
+    (map #(hash-map :BEGIN (take-last len (first %)), :END (take len (last %))) (partition 3 2 (partition-by #(= % :*) (flatten (conj (conj [:BOF] d) :EOF))) ))
+)
+
+(defn diff-detect
+    "Get diff and get chunks"
+    ([src dst] (diff-detect-len src dst 20))
+    ([src dst len] (detect (diff src dst) len))
 )
