69ef0eb6063e7fe3a3a742e79f7b884f2a8d5029
sng2c <sng2nara@gmail.com>
2015-09-07 19:05:23 +0900

    fix to work with String

--- src/template_reverse/core.clj
+++ src/template_reverse/core.clj
@@ -55,25 +55,29 @@
   ([src dst len] (detect (diff src dst) len)))
 
 
+(declare count-contains get-count-item key-frequency find-key-frequency)
 
-
-(defn- count-contains [src coll] (count (filter #{src} coll)))
-(defn- get-count-item
+(defn- -count-contains [src coll] (count (filter #{src} coll)))
+(defn- -get-count-item
   [key coll n]
   (let [keylen (count key)
         parts (partition keylen (+ keylen n) coll)
-        cnt (count-contains key parts)]
+        cnt (-count-contains key parts)]
     {:off n, :all (= cnt (count parts)), :cnt cnt})
   )
 
-(defn key-frequency [key coll]
-  (let [nums (range (inc (- (count coll) (count key))))]
-    (for [n nums, :let [item (get-count-item key coll n)] :when (and (:all item) (< 1 (:cnt item)))]
+(defn key-frequency
+  "Get Frequencies by offset of the key(coll) from first of coll"
+  [key coll]
+  (let [keyseq (seq key), collseq (seq coll), nums (range (inc (- (count collseq) (count keyseq))))]
+    (for [n nums, :let [item (-get-count-item keyseq collseq n)] :when (and (:all item) (< 1 (:cnt item)))]
       (dissoc item :all)
       )))
 
 
-(defn find-key-frequency [coll]
+(defn find-key-frequency
+  "Get All frequencies by offset of all available key(coll)s from first of coll"
+  [coll]
   (let [lastn (quot (count coll) 2)
         nums (range 1 (inc lastn))]
     (for [n nums :let [key (take n coll), freq (key-frequency key coll)] :when (not (empty? freq))]
--- test/template_reverse/core_test.clj
+++ test/template_reverse/core_test.clj
@@ -3,7 +3,7 @@
             [template-reverse.core :refer :all]))
 
 (deftest diff-test
-  (testing "diff 결과테스트"
+  (testing "diff test"
     (is (= '(\A \B \C) (diff "ABC" "ABC")))
     (is (= '(\A \B :*) (diff "ABC" "AB")))
     (is (= '(\A \B :*) (diff "ABC" "ABD")))
@@ -12,20 +12,38 @@
     (is (= '(\A :* \C :* \E) (diff "ABCDE" "A123CE")))
     ))
 
-
-
 (deftest freq-test
   (testing "freq test"
-    (is (= '({:off 2, :cnt 2} {:off 3, :cnt 2})
-           (key-frequency (seq "A") (seq "AABAAC"))))
+    (is (= '(
+              {:off 2, :cnt 2}                              ; '(A ? ? A ? ?)
+              {:off 3, :cnt 2})                             ; '(A ? ? ? A ?)
+           (key-frequency "A" "AABAAC")))
 
-    (is (= '({:off 0, :cnt 2})
-           (key-frequency (seq "AAAA") (seq "AAAAAAAA"))))
+    (is (= '(
+              {:off 0, :cnt 2})                             ; '(AAAA AAAA)
+           (key-frequency "AAAA" "AAAAAAAA")))
 
-    (is (= '({:key (\A), :freq ({:off 0, :cnt 4} {:off 1, :cnt 2} {:off 2, :cnt 2})} {:key (\A \A), :freq ({:off 0, :cnt 2})})
-           (find-key-frequency (seq "AAAA"))))
+    (is (= '(
+              {
+               :key  (\A),
+               :freq (
+                       {:off 0, :cnt 4}                     ; '(A A A A) 4 repetitions by offset 0
+                       {:off 1, :cnt 2}                     ; '(A ? A ?) 2 repetitions by offset 1
+                       {:off 2, :cnt 2})}                   ; '(A ? ? A) 2 repetitions by offset 2
+              {:key  (\A \A),
+               :freq (
+                       {:off 0, :cnt 2})})                  ; '(AA AA) 2 repetitions by offset 0
+           (find-key-frequency "AAAA")))
 
-    (is (= '({:key (\A), :freq ({:off 2, :cnt 3} {:off 5, :cnt 2})} {:key (\A \B), :freq ({:off 1, :cnt 3} {:off 4, :cnt 2})})
-           (find-key-frequency (seq "AB1AB2AB"))))
+    (is (= '(
+              {:key  (\A),
+               :freq (
+                       {:off 2, :cnt 3}
+                       {:off 5, :cnt 2})}
+              {:key  (\A \B),
+               :freq (
+                       {:off 1, :cnt 3}                     ; '(AB ? AB ? AB)
+                       {:off 4, :cnt 2})})                  ; '(AB ? ?? ? AB)
+           (find-key-frequency "AB1AB2AB")))
     ))
 
